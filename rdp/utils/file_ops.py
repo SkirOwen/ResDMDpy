925b0e4f74a6336f339546801c057d10ab60cd48
Owen Allemang <owen.allemang@gmail.com>
2023-05-17 15:24:02 +0100

    [WIP] function to get url of files to download.

--- rdp/utils/file_ops.py
+++ rdp/utils/file_ops.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 import os
 
+from typing import Iterable
+
 from rdp.utils.downloader import downloader
 
 
@@ -12,8 +14,31 @@ def guarantee_file_dl(filepath: str):
 
 	if not os.path.exists(filepath):
 		url = get_url(filename)
-		downloader(url, )
+		downloader(url, root=directory)
 
 
-def get_url(filename: str):
-	pass
+DL_URL = {
+	"Cylinder_data.mat": "",
+	"Cylinder_DMD.mat": "",
+	"Cylinder_EDMD.mat": "",
+	"dataset_1s_b.mat": "",
+	"dataset_1s.mat": "",
+	"double_pendulum_data.mat": "",
+	"EDMD_canopy_final.mat": "",
+	"HotWireData_Baseline.mat": "",
+	"HotWireData_FlowInjection.mat": "",
+	"LIP_times.mat": "",
+	"LIP.mat": "",
+	"mpEDMD_turbulent_data.mat": "",
+	"pendulum_data.mat": "",
+
+}
+
+
+def get_url(filename: str) -> Iterable:
+	# filename = filename.lower()
+	try:
+		url = DL_URL[filename]
+	except KeyError:
+		raise ValueError(f"{filename} is not known.")
+	return [url]
