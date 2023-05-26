5e51021c3b7e46c339b7225687412d1207deb3a2
Owen Allemang <owen.allemang@gmail.com>
2023-05-26 19:06:21 +0100

    added get_example_dir()

---
+++ rdp/utils/directories.py
@@ -0,0 +1,9 @@
+
+import os
+
+from rdp.config import get_rdp_dir
+from rdp.utils.file_ops import guarantee_existence
+
+
+def get_example_dir() -> str:
+	return guarantee_existence(os.path.join(get_rdp_dir(), "rdp", "examples"))
