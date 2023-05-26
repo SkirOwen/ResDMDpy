c044834158f71d69e967077cbb370f5979e56740
Owen Allemang <owen.allemang@gmail.com>
2023-05-26 19:08:32 +0100

    added guarantee_existence() for files

--- rdp/utils/file_ops.py
+++ rdp/utils/file_ops.py
@@ -2,9 +2,26 @@ from __future__ import annotations
 
 import os
 
-from typing import Iterable
+from rdp import logger
+from rdp.utils.downloader import downloader, get_url
 
-from rdp.utils.downloader import downloader
+
+def guarantee_existence(path: str) -> str:
+	"""Function to guarantee the existence of a path, and returns its absolute path.
+
+	Parameters
+	----------
+	path : str
+		Path (in str) to guarantee the existence.
+
+	Returns
+	-------
+	str
+		The absolute path.
+	"""
+	if not os.path.exists(path):
+		os.makedirs(path)
+	return os.path.abspath(path)
 
 
 # TODO: should I use pathlib???
