66f08f630203bac830d7dca8d394958ed3f3b62b
Owen <owen.allemang@gmail.com>
2023-05-30 22:16:26 +0100

    fixed the saving to file with h5 and used ** to pass the metadata around

--- rdp/utils/file_ops.py
+++ rdp/utils/file_ops.py
@@ -42,16 +42,17 @@ def guarantee_file_dl(filepath: str, url: None | list = None) -> None:
 		downloader(url, root=directory)
 
 
-def save_np(filename: str, data: np.ndarray, metadata: dict) -> None:
+def save_np(filename: str, data: np.ndarray, **metadata: dict) -> None:
 	"""Save data to a npz file alongside with the metadata"""
 	np.savez(filename, data=data, **metadata)
 
 
-def save_h5(filename: str, data: np.ndarray, metadata: dict) -> None:
+def save_h5(filename: str, data: np.ndarray, **metadata: dict) -> None:
 	"""Save data to a h5 file alongside the metadata"""
-	with h5py.File(filename, data=data) as f:
+	with h5py.File(filename, "w") as f:
+		f.create_dataset("data", data=data)
 		for k, v in metadata.items():
-			f.attribute[k] = v
+			f.attrs[k] = v
 
 
 def save_data(filename: str, data: np.ndarray, metadata: dict, backend: Literal["numpy", "h5"]) -> None:
@@ -59,4 +60,4 @@ def save_data(filename: str, data: np.ndarray, metadata: dict, backend: Literal[
 	if backend == "numpy":
 		save_np(filename, data, **metadata)
 	elif backend == "h5":
-		save_h5(filename, data, metadata)
+		save_h5(filename, data, **metadata)
