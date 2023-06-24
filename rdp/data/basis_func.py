364f1194673673485038da9f959883407d69152c
Owen <owen.allemang@gmail.com>
2023-06-24 14:25:07 +0100

    testing and removing TODOS and adding precision

--- rdp/data/basis_func.py
+++ rdp/data/basis_func.py
@@ -16,27 +16,34 @@ def gen_from_file(
 	data = loadmat(filepath)
 	ind1 = np.arange(0, m1) + 6000    # slicing in matlab include the last item
 	ind2 = np.arange(0, m2) + (m1 + 6000) + 500
-	# TODO: this slicing returns the right thing, but I think there is a nicer way
-	# I had to do a +1 for ind2
+	# this slicing returns the right thing, but I think there is a nicer way!
+	# Since MATLAB index start at 1, but Python start at zero,
+	# these are off by one, but it is expected .
 
 	if linear_dict == 1:
 		# Linear dictionary
 		_, s, vh = np.linalg.svd(data["DATA"][:, ind1].T / np.sqrt(m1), full_matrices=False)
-		# TODO: during the svd, at least two columns of vh (2, and 4) have their sign flipped compare to matlab
+		# during the svd, at least two columns of vh (2, and 4) have their sign flipped compare to matlab
+		# This is fine as the span from an eigen vector does not change if the sign changes
 		# the vh returned by svd is the hermitian of the v in matlab
-		# TODO: check if having vh instead of v is important
 		psi_x = data["DATA"][:, ind2].T     @ vh.conj().T[:, :n] @ np.diag(1 / s[:n])
 		psi_y = data["DATA"][:, ind2 + 1].T @ vh.conj().T[:, :n] @ np.diag(1 / s[:n])
 	else:
 		psi_x, psi_y, _ = kernel_resdmd(
-			data["DATA"][:, ind1],
-			data["DATA"][:, ind1 + 1],
-			data["DATA"][:, ind2],
-			data["DATA"][:, ind2 + 1],
+			xa=data["DATA"][:, ind1],
+			ya=data["DATA"][:, ind1 + 1],
+			xb=data["DATA"][:, ind2],
+			yb=data["DATA"][:, ind2 + 1],
 			n=n,
 			cut_off=False,
 		)
 
 	return psi_x, psi_y
-		
-		
+
+
+def main():
+	gen_from_file("./rdp/examples/Cylinder_data.mat", use_dmd=0)
+
+
+if __name__ == "__main__":
+	main()
