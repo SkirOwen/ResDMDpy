f80edbb7a6824759326d233821f5e520e983bae2
Owen <owen.allemang@gmail.com>
2023-07-02 15:21:00 +0100

    setup latex in graphs

--- rdp/utils/plotting.py
+++ rdp/utils/plotting.py
@@ -27,20 +27,20 @@ def plot_pseudospectra(D, RES, X, Y, x_pts, y_pts):
 	ax.set_ylim(y_pts[0], y_pts[-1])
 	ax.set_xlim(x_pts[0], x_pts[-1])
 	ax.plot(np.real(D), np.imag(D), '.r')
-	ax.set_xlabel('Real axis')
-	ax.set_ylabel('Imaginary axis')
-	ax.set_title('Title')
+	ax.set_xlabel(r'$\mathrm{Real \: axis}$')
+	ax.set_ylabel(r'$\mathrm{Imaginary \: axis}$')
 	fig.tight_layout()
 	# TODO: I would like less padding on the right
+	plt.savefig("pseudospectra.svg", format="svg")
 	plt.show()
 
 
 def plot_eig_res(D, RES2):
 	fig, ax = plt.subplots()
 	ax.semilogy(np.angle(D), RES2, ".r", markersize=5)
-	ax.set_xlabel('Real axis')
-	ax.set_ylabel(r"Angle")  # TODO: check this
-	ax.set_title('Title')
+	ax.set_xlabel(r'$\mathrm{Real \: axis}$')
+	ax.set_ylabel(r"$\mathrm{Angle}$")  # TODO: check this
+	plt.savefig("eig_res.svg", format="svg")
 	plt.show()
 
 
@@ -61,6 +61,7 @@ def plot_error(lam1, ang1, res1):
 	plt.ylim([10 ** (-14), 1])
 	# plt.yticks(10 ** (np.arange(14, 2, 2))) TODO: fix this
 	plt.xlim([0, 100])
+	plt.savefig("error.svg", format="svg")
 	plt.show()
 
 
